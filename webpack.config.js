52d25c219d7d9e0b5855c77bc73d9f5de6581b7e
rain0104 <b1ack_rain@mail.ru>
2016-10-14 12:24:10 +0300

    updated package.json and webpack.config.js added .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
-var webpack = require('webpack');
-var path = require('path');
-
-var BUILD_DIR = path.resolve(__dirname, 'public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
-
-var config = {
-    entry: APP_DIR + '/index.jsx',
+module.exports = {
+    entry: './src/client.js',
     output: {
-        path: BUILD_DIR,
+        path: './public',
         filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.json']
     }
 };
-
-module.exports = config;
